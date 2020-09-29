1b0dc6197a044d6cf73faa06da196ded333c3964
WittVitorHugo <66031467+WittVitorHugo@users.noreply.github.com>
2020-09-29 15:54:14 -0300

    first commit

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 import Layout from "../../components/layout";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faKey, faCheckCircle, faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
@@ -20,23 +20,33 @@ const Login = () => {
             <div className="login-icon admin" ></div>
           </div>
           <form className="login-form">
-            <div className="login-input-container">
-              <FontAwesomeIcon className="login-input-icon" icon={faUser} />
-              <input
-                className="login-input-field"
-                type="text"
-                placeholder="Login"
-                name="login"
-              ></input>
+            <div className="input-validation-container" >
+              <div className="login-input-container login">
+                <FontAwesomeIcon className="login-input-icon" icon={faUser} />
+                <input
+                  className="login-input-field"
+                  type="text"
+                  placeholder="Login"
+                  name="login"
+                ></input>
+                <FontAwesomeIcon className="login-icon" icon={faCheckCircle} />
+                <FontAwesomeIcon className="login-icon" icon={faExclamationCircle} />
+              </div>
+              <small>Erro</small> 
             </div>
-            <div className="login-input-container">
-              <FontAwesomeIcon className="login-input-icon" icon={faKey} />
-              <input
-                className="login-input-field"
-                type="password"
-                placeholder="Senha"
-                name="senha"
-              ></input>
+            <div className="input-validation-container" >
+              <div className="login-input-container  senha">
+                <FontAwesomeIcon className="login-input-icon" icon={faKey} />
+                <input
+                  className="login-input-field"
+                  type="password"
+                  placeholder="Senha"
+                  name="senha"
+                ></input>
+                <FontAwesomeIcon className="login-icon" icon={faCheckCircle} />
+                <FontAwesomeIcon className="login-icon" icon={faExclamationCircle} />
+              </div>
+              <small>Erro</small> 
             </div>
             <Link to="/forgot-password">Esqueceu sua senha?</Link>
             <Link to="/register">Criar conta</Link>
