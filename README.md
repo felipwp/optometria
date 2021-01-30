bf6dd93255652b6678fb69fb21727e7c71555f30
Felipe Pereira <felipwp@hotmail.com>
2021-01-30 00:29:16 -0300

    Adicionando README e atualizando configurações do firebase

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+<img alt="E-commerce Boilerplate" src="https://i.imgur.com/KnWfwss.png"/>
+
+<h3 align="center">
+    Optometria
+</h3>
+
+<p align="center">Sistema para clínica de optometria</p>
+
+## Sobre 
+
+Aplicação desenvolvida na matéria de Estagio Supervisionado, do curso de Engenharia de Software (5º e 6º período).
+
+### Funcionalidades
+
+Principais funcionalidades da aplicação: 
+
+- CRUD de usuários, médicos e pacientes
+- Sistema de login e níveis de usuário
+
+
 ## Scripts
 
 No diretório do projeto, você pode rodar os seguintes comandos:
@@ -9,3 +29,13 @@ Para iniciar o projeto em modo de desenvolvimento.
 ### `npm run build`
 
 Para gerar uma versão para produção e suas dists.
+
+## Tecnologias Utilizadas 
+
+- React
+- NodeJS
+- ElectronJS
+- Firebase
+
+<img alt="Página de Login" src="https://i.imgur.com/lvgwRTW.png"/>
+<img alt="Dashboard" src="https://i.imgur.com/EnFumIt.png"/>
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
-import firebase from "firebase";
+  import firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyBR5hiq1aMgw5R8DNvMw4diQCGIwsYlRSk",
-  authDomain: "optometria-ef150.firebaseapp.com",
-  databaseURL: "https://optometria-ef150.firebaseio.com",
-  projectId: "optometria-ef150",
-  storageBucket: "optometria-ef150.appspot.com",
-  messagingSenderId: "624404834393",
-  appId: "1:624404834393:web:983baa73bdd7eccf022948"
-};
+  const config = {
+    apiKey: "AIzaSyBR5hiq1aMgw5R8DNvMw4diQCGIwsYlRSk",
+    authDomain: "optometria-ef150.firebaseapp.com",
+    databaseURL: "https://optometria-ef150.firebaseio.com",
+    projectId: "optometria-ef150",
+    storageBucket: "optometria-ef150.appspot.com",
+    messagingSenderId: "624404834393",
+    appId: "1:624404834393:web:983baa73bdd7eccf022948"
+  };
 
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-export default firebase;
+  export default firebase;
+
+  
